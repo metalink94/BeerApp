0455a0e775d2243844763bfb6c6bb91bde47a353
DESKTOP-O1LOM0Q\Денис <denis.novikov@akbarsdigital.ru>
2016-12-21 10:07:29 +0300

    add login

---
+++ app/src/main/java/ru/beer/beerapp/baseUtils/BaseActivity.java
@@ -0,0 +1,18 @@
+package ru.beer.beerapp.baseUtils;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Денис on 21.12.2016.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
